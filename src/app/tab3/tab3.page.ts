f039e742662b00262cfbd896b01147a7f6a41541
Ariana Gonzabay <arigalda@espol.edu.ec>
2025-01-14 16:10:15 -0500

    Modificación en la pestaña -Acerca de-

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, 
+         IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { leafOutline, analyticsOutline, peopleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonCard, 
+    IonCardHeader, 
+    IonCardSubtitle, 
+    IonCardTitle, 
+    IonCardContent, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonIcon,
+    ExploreContainerComponent
+  ],
 })
 export class Tab3Page {
   constructor() {}
+  public leafIcon = leafOutline;
+  public analyticsIcon = analyticsOutline;
+  public peopleIcon = peopleOutline;
 }
